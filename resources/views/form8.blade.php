c6d973ac88834c599ee9c310472a06aca0419386
Akinori <aki10.working@gmail.com>
2021-02-16 23:07:52 +0900

    add img_path null in forms

--- resources/views/form8.blade.php
+++ resources/views/form8.blade.php
@@ -11,11 +11,11 @@
             <x-slot name="title">Q.12. 担当者の対応は *</x-slot>
             <x-input-error name="answer.about_staff" />
 
-            <x-checkbox type="radio" name="answer[about_staff]" value="満足している" id="check0" str="満足している" key="about_staff" img_path="" />
-            <x-checkbox type="radio" name="answer[about_staff]" value="やや満足している" id="check1" str="やや満足している" key="about_staff" img_path="" />
-            <x-checkbox type="radio" name="answer[about_staff]" value="どちらともいえない" id="check2" str="どちらともいえない" key="about_staff" img_path="" />
-            <x-checkbox type="radio" name="answer[about_staff]" value="あまり満足していない" id="check3" str="あまり満足していない" key="about_staff" img_path="" />
-            <x-checkbox type="radio" name="answer[about_staff]" value="満足していない" id="check4" str="満足していない" key="about_staff" img_path="" />
+            <x-checkbox type="radio" name="answer[about_staff]" value="満足している" id="check0" str="満足している" key="about_staff" imgpath="" />
+            <x-checkbox type="radio" name="answer[about_staff]" value="やや満足している" id="check1" str="やや満足している" key="about_staff" imgpath="" />
+            <x-checkbox type="radio" name="answer[about_staff]" value="どちらともいえない" id="check2" str="どちらともいえない" key="about_staff" imgpath="" />
+            <x-checkbox type="radio" name="answer[about_staff]" value="あまり満足していない" id="check3" str="あまり満足していない" key="about_staff" imgpath="" />
+            <x-checkbox type="radio" name="answer[about_staff]" value="満足していない" id="check4" str="満足していない" key="about_staff" imgpath="" />
         </x-form-group>
 
         <input type="hidden" name="next" value="form9">
